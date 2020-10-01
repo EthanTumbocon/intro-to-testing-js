375e2ef22c67b1550491446d73373a5e9f0d0bc4
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-10-01 14:19:25 -0500

    wrote my first test sayHello function

--- code.js
+++ code.js
@@ -2,3 +2,14 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+// const helloWorld = function(){
+//     return "Hello, World!";
+// }
+
+
+//sayHello function
+
+function sayHello(){
+
+}
