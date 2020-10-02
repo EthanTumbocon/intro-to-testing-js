31877efae7c739cc7089f5d91498cf55fed2511b
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-10-01 21:03:42 -0500

    finished JS testing exercises

--- code.js
+++ code.js
@@ -26,9 +26,21 @@ function isEven(x) {
     return x % 2 === 0 && typeof x !== 'boolean';
 }
 
+function isVowel(letter){
+    switch(letter){
+        case "a": case "A": case "e": case "E": case "i": case "I": case "o": case "O": case "u": case "U":
+            return true;
+    }
+    return false;
+}
 
-
-
+function add(a,b){
+    if(isNaN(a) || isNaN(b)){
+        return "NaN";
+    }else {
+        return Number(a) + Number(b);
+    }
+}
 
 
 
